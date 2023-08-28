0ab62884486cf1498731f7a49c6a9583310c40e6
Darius Bowser <134982506+DariusBowser@users.noreply.github.com>
2023-08-28 17:54:11 -0400

    initial home design

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./HomePage.css"; // Make sure to import your CSS file
 
 export default function HomePage() {
   return (
-    <>
+    <div className="home-page">
       <h1>Home</h1>
-    </>
+      <div className="card-container">
+        <Link to="/visits" className="card card-1">
+          <span className="card-text">Visits</span>
+        </Link>
+        <Link to="/posts" className="card card-2">
+          <span className="card-text">Posts</span>
+        </Link>
+      </div>
+    </div>
   );
 }
