0e5cd3dbd1a9b19c0afdc5d7279ed92d75c95f87
Darius Bowser <134982506+DariusBowser@users.noreply.github.com>
2023-08-28 11:54:46 -0400

    chat design

--- src/components/ChatWindow/ChatWindow.jsx
+++ src/components/ChatWindow/ChatWindow.jsx
@@ -1,93 +1,4 @@
-// import io from "socket.io-client";
-// import { IconButton, Paper, TextField, Typography } from "@mui/material";
-// import SendIcon from "@mui/icons-material/Send";
-// import { Box } from "@mui/system";
-// import { useState, useEffect } from "react";
-// import {
-//   createChatMessage,
-//   getAllChatMessages,
-// } from "../../utilities/chat-api";
 
-// const socket = io.connect("http://localhost:3001");
-
-// export default function ChatWindow({ user }) {
-//   const [messages, setMessages] = useState([]);
-//   const [message, setMessage] = useState("");
-
-//   // console.log("Profile:", user);
-
-//   const chatName = user
-//     ? (user.useUsername
-//       ? user.username
-//       : user.firstName + " " + user.lastName)
-//     : "";
-
-//     // console.log("Chat Name:", chatName);
-
-//   function sendMessage() {
-//     const userMessage = {
-//       nameOfUser: chatName,
-//       message,
-//     };
-//     socket.emit("send_message", userMessage);
-//     setMessages([...messages, userMessage]);
-//     setMessage("");
-//     createChatMessage(userMessage);
-//   }
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       const response = await getAllChatMessages();
-//       const responseMessages = response.data.map((m) => {
-//         return { nameOfUser: m.nameOfUser, message: m.message };
-//       });
-//       setMessages(responseMessages);
-//     }
-//     fetchData();
-//   }, []);
-
-//   useEffect(() => {
-//     socket.on("receive_message", (data) => {
-//       setMessages([...messages, data]);
-//     });
-//   }, [messages]);
-
-//   return (
-//     <Box id="chat-window">
-//       <Box m={2}>
-//         {messages.map((m, i) => (
-//           <Typography
-//             style={{ color: m.nameOfUser === chatName ? "green" : "blue" }}
-//             key={i}
-//             variant="h6"
-//           >
-//             {`${m.nameOfUser}: ${m.message}`} <br />
-//           </Typography>
-//         ))}
-//       </Box>
-//       <Paper
-//         sx={{
-//           p: "2px 4px",
-//           display: "flex",
-//           alignItems: "center",
-//           width: 500,
-//           position: "absolute",
-//           bottom: 0,
-//         }}
-//       >
-//         <TextField
-//           sx={{ ml: 1, flex: 1 }}
-//           label="Enter Message..."
-//           value={message}
-//           onChange={(e) => setMessage(e.target.value)}
-//         />
-//         <IconButton type="button" onClick={sendMessage}>
-//           <SendIcon />
-//         </IconButton>
-//       </Paper>
-//     </Box>
-//   );
-// }
 
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
@@ -97,9 +8,13 @@ import {
   TextField,
   Typography,
   InputAdornment,
+  Badge,
+  Avatar,
+
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import SendIcon from "@mui/icons-material/Send";
+import MailIcon from "@mui/icons-material/Mail";
 import { Box } from "@mui/system";
 import {
   createChatMessage,
@@ -154,95 +69,119 @@ export default function ChatWindow({ profile }) {
   };
 
   return (
-    <div>
-      {!chatVisible && (
+  <div>
+    {!chatVisible && (
+      <div
+        onClick={toggleChatVisible}
+        style={{
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          backgroundColor: "green",
+          color: "white",
+          padding: "10px",
+          cursor: "pointer",
+          borderRadius: "8px",
+          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        Open Chat
+      </div>
+    )}
+
+    {chatVisible && (
+      <Box
+        id="chat-window"
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          right: 0,
+          zIndex: 999,
+          backgroundColor: "white",
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+          width: "500px",
+          maxHeight: "70%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
         <div
-          onClick={toggleChatVisible}
           style={{
-            position: "fixed",
-            bottom: "20px",
-            right: "20px",
-            backgroundColor: "blue",
-            color: "white",
+            display: "flex",
+            justifyContent: "space-between", // Adjusted alignment
+            alignItems: "center", // Center the message icon vertically
             padding: "10px",
-            cursor: "pointer",
+            borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
           }}
         >
-          Open Chat
+          <Typography variant="h6">Happy Chatting</Typography>
+          <IconButton onClick={closeChat}>
+            <CloseIcon />
+          </IconButton>
         </div>
-      )}
-
-      {chatVisible && (
         <Box
-          id="chat-window"
+          m={2}
           sx={{
-            position: "fixed",
-            bottom: 0,
-            right: 0,
-            zIndex: 999,
-            backgroundColor: "white",
-            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
-            width: "500px",
-            maxHeight: "70%",
             display: "flex",
             flexDirection: "column",
+            gap: "10px",
+            maxHeight: "calc(70% - 60px)", // Adjusted to account for header height
+            overflowY: "auto",
           }}
         >
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              padding: "10px",
-            }}
-          >
-            <IconButton onClick={closeChat}>
-              <CloseIcon />
-            </IconButton>
-          </div>
-          <Box m={2}>
-            {messages.map((m, i) => (
+          {messages.map((m, i) => (
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginBottom: "10px",
+              }}
+              key={i}
+            >
+              <Avatar src={m.avatar} alt={m.nameOfUser} /> {/* Display the avatar */}
               <Typography
                 style={{
+                  marginLeft: "10px",
                   color: m.nameOfUser === chatName ? "green" : "blue",
                 }}
-                key={i}
-                variant="h6"
+                variant="body1"
               >
-                {`${m.nameOfUser}: ${m.message}`} <br />
+                <strong>{m.nameOfUser}</strong>: {m.message}
               </Typography>
-            ))}
-          </Box>
-          <Paper
-            sx={{
-              p: "2px 4px",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <TextField
-              sx={{ ml: 1, flex: 1 }}
-              label="Enter Message..."
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton type="button">
-                      <SendIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              onKeyDown={(e) => {
-                if (e.key === "Enter" && !e.shiftKey) {
-                  e.preventDefault();
-                  sendMessage(e);
-                }
-              }}
-            />
-          </Paper>
+            </div>
+          ))}
         </Box>
-      )}
-    </div>
-  );
+        <Paper
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <TextField
+            sx={{ ml: 1, flex: 1 }}
+            label="Enter Message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton type="button" onClick={sendMessage}>
+                    <SendIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                sendMessage(e);
+              }
+            }}
+          />
+        </Paper>
+      </Box>
+    )}
+  </div>
+);
 }
