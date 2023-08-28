2759a44266d8f2fbd65469dc2dc8f09a8cf3e7be
jabbacalvin56 <jabbacalvin@gmail.com>
2023-08-28 14:29:10 -0500

    working on profile page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,3 +1,7 @@
+import ProfileDetails from "../../components/ProfileDetails/ProfileDetails";
+import PostContainer from "../../components/PostContainer/PostContainer";
+import { Box, Grid, Paper } from "@mui/material/";
+
 export default function ProfilePage({
   updatingProfile,
   setUpdatingProfile,
@@ -7,6 +11,23 @@ export default function ProfilePage({
   return (
     <>
       <h1>Profile</h1>
+      <Grid container spacing={2}>
+        <Grid item xs={9}>
+          <Paper>
+            <PostContainer />
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper sx={{ p: 2 }}>
+            <ProfileDetails
+              updatingProfile={updatingProfile}
+              setUpdatingProfile={setUpdatingProfile}
+              profile={profile}
+              setProfile={setProfile}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   );
 }
